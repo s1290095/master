09dcfd243b254dc96a7f53b5449b84e75d9319c5
佐藤惣一朗 <sekiakujua112233@gmail.com>
2022-07-11 13:07:46 +0900

    Final version

--- DataManagement/src/excersise/ex01/HeadsOrTails.java
+++ DataManagement/src/excersise/ex01/HeadsOrTails.java
@@ -1,10 +1,15 @@
 package excersise.ex01;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class HeadsOrTails {
 	public static void main(String args[]) {
-		System.out.println("コイン投げ");
+		System.out.println("Who are you?");
+		Scanner sc = new Scanner(System.in);
+		String name = sc.next();
+		System.out.println("Hello!" + name + "!");
+		System.out.println("Tossing a coin...");
 		
 		Random random = new Random();
 		int cntHead = 0;
@@ -12,16 +17,23 @@ public class HeadsOrTails {
 		
 		for(int i=0;i<3;i++) {
 			int coin = random.nextInt(5);
-			System.out.println("Round-" + i+1);
 			if(coin % 2 == 0) {
-				System.out.println("HEAD");
+				System.out.println("Round-" + (i+1) + ": HEAD");
 				cntHead++;
 			}
 			else {
-				System.out.println("TAIL");
+				System.out.println("Round-" + (i+1) + ": TAIL");
 				cntTail++;
 			}
 		}
 		System.out.println("HEAD: " + cntHead + ", " + "TAIL: " + cntTail);
+		
+		if(cntHead > cntTail) {
+			System.out.println(name + " won!");
+		}
+		else {
+			System.out.println(name + " lose!");
+		}
+		sc.close();
 	}
 }
